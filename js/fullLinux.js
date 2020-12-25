bc0e2402858a290987e6ca98d7474355d0c0067d
Manthee1 <dinja.martin@gmail.com>
2020-12-25 23:48:08 +0100

    ➕ Added an App Menu
    ➕ Added an App Menu
    💅 Optimized the GUI system
    🔧 Fixed errors with terminal commands
    🔁 Revamped the Design

--- js/fullLinux.js
+++ js/fullLinux.js
@@ -11,131 +11,97 @@ system = {
 		elementExists: (el) => {
 			if (typeof el != "undefined" && el != null) return true;
 			else return false;
-		}
+		},
+		escapeHtml: (text) => {
+			var map = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#039;' };
+			return text.replace(/[&<>"']/g, function (m) { return map[m]; });
+		},
 	},
+
+	changeBrightness: function (brightness) {
+		this.global.brightness = brightness;
+		document.querySelector("html").style.filter = `brightness(${system.global.brightness / 100})`;
+	},
+	changeVolume: function (volume) {
+		this.global.volume = volume;
+		let img
+
+		X.statusArea.updateVolume();
+
+	},
+
 	startup: function () {//  Important: Should only be run once
 		if (this.started) return false;
 		this.started = true;
-
 		popupContainer = document.querySelector("system_popup_container");
-		appsLayer = document.querySelector("apps_layer")
+		appsLayer = document.querySelector("apps_layer");
 		desktop = document.querySelector("background_wallpaper");
 		processBar = document.querySelector("process_bar");
-
-
+		system.openPopups = [];
 		//Add system.build variable
 		(async () => {
 			system.build = await (async () => { return (await fetch("./build.ver")).text() })()
 		})()
 
-		document.body.addEventListener("click", () => {
-			if (system.global.elementExists(document.querySelector("status_area_container"))) {
-				statusArea.volumeSliderDisplayToggle(true);
-				popupContainer.innerHTML = "";
-			}
-		});
-
-		statusArea.updateStatusAreaTime();
+		X.statusArea.updateStatusAreaTime();
 		setTimeout(() => {
 			setInterval(() => {
-				statusArea.updateStatusAreaTime();
+				X.statusArea.updateStatusAreaTime();
 			}, 60 * 1000);
 		}, (60 - new Date().getSeconds()) * 1000); //makes sure the update is synchronized
-		statusArea.statusAreaButton.addEventListener("click", (event) => {
-			event.stopPropagation();
-			statusArea.volumeSliderDisplayToggle();
-		});
 
-		desktop.addEventListener("contextmenu", (event) => {
-			event.preventDefault();
-			var x = event.clientX;
-			var y = event.clientY;
-			if (system.global.elementExists(popupContainer.querySelector("context_menu")))
-				popupContainer.querySelector("context_menu").remove();
-			popupContainer.innerHTML += `
-		<context_menu style="top: ${y}px;left: ${x}px;">
-				<context_item onclick="processes.create('terminal', {x:${x},y:${y}});">Terminal</context_item>
-				<context_item onclick="processes.create('google', {x:${x},y:${y}});">Google</context_item>
-				<context_item onclick="processes.create('linuxWeb', {x:${x},y:${y}});">linuxWeb</context_item>
-				<context_item>Other</context_item>
-			</context_menu>
-				`;
-			document.body.addEventListener("click", () => {
-				if (system.global.elementExists(popupContainer.querySelector("context_menu"))) {
-					popupContainer.querySelector("context_menu").remove();
-				}
-			});
-			return false;
-		},
-			false
-		);
+		Object.values(X).forEach(xObj => { if (typeof (xObj) == "object" && typeof (xObj['enable']) == "function") { xObj.enable() } })
+		Object.entries(X).forEach(xObj => {
+			console.log(xObj);
+			if (xObj[1].toggleButtonElement != undefined) {
+				console.log(xObj, "set");
+				xObj[1].toggleButtonElement.addEventListener('click', () => {
+					if (!system.openPopups.includes(xObj[0])) {
+						setTimeout(() => {
+							popupContainer.innerHTML += xObj[1].getHTML();
 
+							popupContainer.children[popupContainer.children.length - 1].addEventListener('click', (event) => {
+								console.log(event);
+								copyOf_openPopups = [...system.openPopups]
+								system.openPopups = [];
+								setTimeout(() => {
+									system.openPopups = [...copyOf_openPopups]
+									copyOf_openPopups = undefined;
+								}, 1);
+							})
+							xObj[1].toggleButtonElement.style.borderBottom = "solid gray 2px";
+							system.openPopups.push(xObj[0]);
+						}, 1);
+					}
 
-
-	}
-}
-statusArea = {
-
-	statusAreaButton: document.querySelector("statusArea"),
-
-	updateStatusAreaTime: function () {
-		var dateAndTime = new Date().toUTCString().split(new Date().getFullYear())[0] + date.getTime("hm");
-		document.querySelector("dateTime").innerHTML = dateAndTime;
-		console.log("Time update. Next one in: ", 60 - new Date().getSeconds());
-	},
-
-	addListenerForBrightnessChange: function () {
-		brightnessSlider = document.querySelector("#brightness_slider");
-		brightnessSlider.addEventListener("input", (event) => {
-			system.global.brightness = brightnessSlider.value;
-			document.querySelector("html").style.filter = `brightness(${system.global.brightness / 100})`;
-		});
-	},
-
-	volumeSliderDisplayToggle: function (forceRemove = false) {
-		if (system.global.elementExists(document.querySelector("status_area_container"))) {
-			statusAreaContainer = document.querySelector("status_area_container");
-			statusAreaContainer.style = "opacity:0;";
-			statusArea.statusAreaButton.style.borderBottom = "";
-
-			setTimeout(() => {
-				statusAreaContainer.remove();
-			}, 300);
-		} else if (!forceRemove) {
-			popupContainer.innerHTML += `
-        <status_area_container>
-            <input id='volume_slider' min="0" max="100" value="${system.global.volume}" step="1" type="range">
-            <input id='brightness_slider' min="25" max="100" value="${system.global.brightness}" step="1" type="range">
-        </status_area_container>`;
-			statusAreaContainer = document.querySelector("status_area_container");
-			statusAreaContainer.style = "opacity:1;";
-			statusAreaContainer.addEventListener("click", (event) => {
-				event.stopPropagation();
-			});
-			statusArea.statusAreaButton.style.borderBottom = "solid gray 2px";
-			statusArea.addListenerForVolumeChange();
-			statusArea.addListenerForBrightnessChange();
-		}
-	},
-
-	addListenerForVolumeChange: function () {
-		let volumeIndicator = document.querySelector("volume");
-		let volumeSlider = document.querySelector("#volume_slider");
-		volumeSlider.addEventListener("input", (event) => {
-			system.global.volume = volumeSlider.value;
-			// document.querySelector("volume_text").innerHTML = volumeSlider.value;
-			if (volumeSlider.value > 66) {
-				volumeIndicator.style.backgroundImage = "url('./img/volume/high.svg')";
-			} else if (volumeSlider.value > 33) {
-				volumeIndicator.style.backgroundImage = "url('./img/volume/medium.svg')";
-			} else if (volumeSlider.value > 0) {
-				volumeIndicator.style.backgroundImage = "url('./img/volume/low.svg')";
-			} else {
-				volumeIndicator.style.backgroundImage = "url('./img/volume/mute.svg')";
+				});
 			}
+
+
+
+		})
+		document.body.addEventListener("click", () => {
+			system.clearOpenPopups()
 		});
+
+
+	},
+
+	clearOpenPopups: function (forcefully = false) {
+		if (system.openPopups.length != 0 || forcefully) {
+			console.log("Body click");
+			system.openPopups.forEach(openPopup => {
+				X[openPopup].toggleButtonElement.style.borderBottom = "";
+			})
+			system.openPopups = [];
+			// X.statusArea.volumeSliderDisplayToggle(true);
+			popupContainer.innerHTML = "";
+		}
+
 	}
 }
+
+
 processes = {
 
 	currentlySelectedProcess: null,
@@ -158,25 +124,26 @@ processes = {
 	bringToTop: function (element) {
 
 		const pid = this.getNumberPid(element.id);
+		typeof (this.pid[pid]['onFocus']) == "function" && this.pid[pid].onFocus();
 		if (this.currentlySelectedProcess == this.pid[pid]) return false;
-
 		// if (element == appsLayer.children[appsLayer.children.length - 1]) return false;
 		appsLayer.insertAdjacentElement('beforeend', element)
 		this.makeProcessResizable("#" + element.id);
 		this.currentlySelectedProcess != null && this.currentlySelectedProcess.getProcessBarElement().classList.remove('selected')
 		this.pid[pid].getProcessBarElement().classList.add('selected');
 		this.currentlySelectedProcess = this.pid[pid];
-		this.pid[pid]['onfocus'] != undefined && this.pid[pid].onFocus();
-
+		console.log('Focused', this.pid[pid]);
 	},
 
 	processSchema: {
 		title: "Untitled App",
-		titleColor: "initial",
-		bodyColor: "initial",
-		textColor: "initial",
-		headerColor: "initial",
-		bodyBorderSize: "initial",
+		titleColor: "inherit",
+		bodyColor: "inherit",
+		textColor: "inherit",
+		headerColor: "var(--main-color);",
+		bodyBorderSize: "inherit",
+		padding: "inherit",
+		opacity: 1,
 		HTML: String(),
 		minWidth: Number(),
 		minHeight: Number(),
@@ -193,10 +160,6 @@ processes = {
 		this.pid[pid].getProcessBarElement().remove();
 		this.pid[pid] = null;
 		this.currentlySelectedProcess = null;
-
-
-
-
 	},
 
 	create: function (appName, position = { x: 100, y: 100 }) {
@@ -227,10 +190,12 @@ processes = {
 			margin:${(appCreateData.bodyBorder && appCreateData.bodyBorderSize) || '0px'};
 			height:${(appCreateData.fullHeight && "100%") || "auto"};
 			width:${(appCreateData.fullWidth && "100%") || "auto"};
+			opacity: ${appCreateData.opacity};
+			padding: ${appCreateData.padding};
 		`
 
 		appsLayer.innerHTML += `
-			<app_container onmousedown="processes.bringToTop(this)" id = '${stringyPID}' style = "top: ${position.y}px;left: ${position.x}px;${containerStyles}" >
+			<app_container onmousedown="processes.bringToTop(this)" id='${stringyPID}' style = "top: ${position.y}px;left: ${position.x}px;${containerStyles}" >
 				<app_header style="color:${appCreateData.titleColor};background-color:${appCreateData.headerColor};" onmousedown="processes.processMouseDownHandler(event, '${stringyPID}')" >
 					${appCreateData.title}
 					<app_exit onmousedown="document.body.removeAttribute('onmousemove')" onclick="processes.remove('${stringyPID}')" />
@@ -282,7 +247,6 @@ processes = {
 		if (event.target.tagName != "APP_HEADER") return false
 		pid = this.getNumberPid(stringyPID);
 		let process = this.pid[pid];
-		console.log(event, process);
 		process.originalOffsetY = event.offsetY;
 		process.originalOffsetX = event.offsetX;
 		document.body.setAttribute('onmousemove', `processes.processMouseMoveHandler(event,processes.pid['${pid}'])`)
@@ -364,40 +328,39 @@ processes = {
 apps = {
 
 	terminal: {
-
+		name: "linuxWeb Terminal",
 		path: "apps.terminal",
-		version: "1.0.3",
+		version: "1.0.4",
 
 		createData: {
 			title: "JS Terminal",
-			titleColor: "white",
 			bodyColor: "black",
-			textColor: "#c5c5c5",
+			textColor: "white",
 			bodyBorder: false,
 			bodyBorderSize: '0px',
+			opacity: '0.9',
+			padding: "10px 5px",
 			HTML: `
-				<terminal_main >Linux terminal version 1.0.3</terminal_main>
-				<terminal_input >LinuxWeb@root:-$<input id type=text></terminal_input>
-`,
+				<terminal_main >Linux terminal version 1.0.3<br></terminal_main>
+				<terminal_input >LinuxWeb@root:-$<input type=text></terminal_input>
+				`,
 			methods: {
 				commandHistory: [],
 				currentHistoryNumber: -1,
 				currentCommand: "",
 				onFocus: function () {
-					console.log('element focused: ', this)
-					let terminalBody = this.getProcessElementBody()
-					this.getProcessElementBody().querySelector('terminal_input > input').focus()
-					console.log(this.getProcessElementBody().querySelector('terminal_input > input'));
+					// let terminalBody = this.getProcessElementBody()
+					//Timeout set in order for the focus to work at all
+					setTimeout(() => {
+						let focusElement = this.getProcessElementBody().querySelector('terminal_input > input')
+						console.log(document.activeElement, focusElement);
+						document.activeElement != focusElement && focusElement.focus();
+					}, 1);
 				},
 				addToCommandHistory: function (commandToPush) {
-					console.log(this);
 					this.commandHistory.length > 30 && this.commandHistory.pop();
 					this.commandHistory.unshift(commandToPush);
-
 				},
-
-
-
 			}
 		},
 
@@ -418,19 +381,21 @@ apps = {
 		},
 
 		parseCommand: async function (event, element, processInstance) {
-			console.log(event.code);
 			if (event.code == 'Enter') {
 				terminalElement = this.InitiateVariables(processInstance);
-				text = element.value
+				text = element.value;
 				element.value = "";
-				terminalElement.main.innerHTML += " <br>" + terminalElement.inputPrefix.innerText + text + "<br>";
+				terminalElement.main.innerHTML += `${system.global.escapeHtml(terminalElement.inputPrefix.innerText)} ${system.global.escapeHtml(text)}<br>`;
 				try {
 					commandExecuted = eval(text);
-					terminalElement.main.innerHTML += commandExecuted.toString().replace(/\n/g, "<br>");
-
+					if (typeof (commandExecuted) != 'undefined') {
+						if (typeof (commandExecuted) != 'object') {
+							commandExecuted = system.global.escapeHtml(commandExecuted.toString()).replace(/\n/g, "<br>");
+						} else commandExecuted = text
+						terminalElement.main.innerHTML += commandExecuted + "<br>";
+					}
 				} catch (e) {
-					terminalElement.main.innerHTML += e;
-
+					terminalElement.main.innerHTML += e + "<br>";
 				}
 				processInstance.addToCommandHistory(text);
 				element.scrollIntoView(false)
@@ -460,6 +425,8 @@ apps = {
 
 
 	linuxWeb: {
+		name: "linuxWeb Websites",
+
 		createData: {
 			title: "linuxWeb",
 			fullHeight: true,
@@ -472,6 +439,7 @@ apps = {
 	},
 
 	google: {
+		name: "Google Websites",
 
 		createData: {
 			title: "Google Search",
@@ -484,5 +452,103 @@ apps = {
 
 	}
 }
+
+X = {
+	desktopContextMenu: {
+		enable: function () {
+			desktop.addEventListener("contextmenu", (event) => {
+				event.preventDefault();
+				var x = event.clientX;
+				var y = event.clientY;
+				if (system.global.elementExists(popupContainer.querySelector("context_menu")))
+					popupContainer.querySelector("context_menu").remove();
+				popupContainer.innerHTML += `
+		<context_menu style="top: ${y}px;left: ${x}px;">
+				<context_item onclick="processes.create('terminal', {x:${x},y:${y}});">Terminal</context_item>
+				<context_item onclick="processes.create('google', {x:${x},y:${y}});">Google</context_item>
+				<context_item onclick="processes.create('linuxWeb', {x:${x},y:${y}});">linuxWeb</context_item>
+				<context_item>Other</context_item>
+			</context_menu>
+				`;
+				document.body.addEventListener("click", () => {
+					if (system.global.elementExists(popupContainer.querySelector("context_menu"))) {
+						popupContainer.querySelector("context_menu").remove();
+					}
+				});
+				return false;
+			},
+				false
+			);
+
+		}
+	},
+
+	appMenu: {
+
+		toggleButtonElement: document.querySelector('app_menu_button'),
+		getHTML: function () {
+			let html = `
+				<app_menu_container>
+					<app_search><input type='search'></app_search>
+					<app_list>
+					${Object.entries(apps).map(x => {
+				let appName;
+				if (x[1].name == undefined) appName = x[0]; else appName = x[1].name;
+				return `<app_list_name onclick="system.clearOpenPopups();processes.create('${x[0]}');">${appName}</app_list_name>`
+			}).join('')}
+			</app_list>
+				</app_menu_container>
+		`
+			return html;
+		},
+	},
+
+	statusArea: {
+
+		toggleButtonElement: document.querySelector("statusArea"),
+
+		getHTML: function () {
+
+			return `<status_area_container>
+					<slider_container>
+					<volume_icon></volume_icon>
+					<input oninput='system.changeVolume(this.value)' id='volume_slider' min="0" max="100" value="${system.global.volume}" step="1" type="range">
+					</slider_container>
+					<slider_container>
+					<brightness_icon></brightness_icon>
+					<input  oninput='system.changeBrightness(this.value)' id='brightness_slider' min="25" max="100" value="${system.global.brightness}" step="1" type="range">
+					</slider_container>
+					<item><settings_icon></settings_icon><text>Settings (*Not Functional)</text></item>
+					<item><padlock_icon></padlock_icon><text>Lock (*Not Functional)</text></item>
+					<item><power_off_icon></power_off_icon><text>Power Off/Log Out (*-||-)</text></item>
+
+        </status_area_container>`;
+		},
+
+		updateStatusAreaTime: function () {
+			var dateAndTime = new Date().toUTCString().split(new Date().getFullYear())[0] + date.getTime("hm");
+			document.querySelector("dateTime").innerHTML = dateAndTime;
+			console.log("Time update. Next one in: ", 60 - new Date().getSeconds());
+		},
+
+		updateVolume: function () {
+			volume = system.global.volume;
+			if (volume > 66) {
+				img = "url('./img/volume/high.svg')";
+			} else if (volume > 33) {
+				img = "url('./img/volume/medium.svg')";
+			} else if (volume > 0) {
+				img = "url('./img/volume/low.svg')";
+			} else {
+				img = "url('./img/volume/mute.svg')";
+			}
+			document.querySelectorAll('volume_icon').forEach(x => x.style.backgroundImage = img)
+		}
+	}
+
+
+}
+
+
 system.startup();
 
